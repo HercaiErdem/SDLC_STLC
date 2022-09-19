105e581f141983c2b954c73025031a19e3b3cee2
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-19 16:22:30 +0300

    19.9.2022

---
+++ src/test/java/ScrumSureci.java
@@ -0,0 +1,89 @@
+public class ScrumSureci {
+
+    // SCRUM SÜRECİ
+
+    //Product Backlog(Ürün İş Listesi) Nedir:
+    /*
+    * Proje/ ürün için ihtiyaç duyulan her şeyin sıralandığı gereksinimler listesidir.
+    * Product Owner(PO) tarafından müşteriden(Stakeholder) gereksinimler(requirement) alınır,
+      öncelik sırasına göre sıralanır.
+
+    * Product Backlog yaşayan bir dokümandır. Çevresel koşullarda, müşteri taleplerinde, teknolojik gelişmelerde
+    meydana gelebilecek değişimlerden dolayı sürekli değişir. Product owner, değişen ihtiyaçları product
+    backlog’a ekleyen veya çıkaran kişidir.
+    * PO, Product Backlog’u yöneten yegane kişidir. Bu işi yaparken development team den ve Scrum
+    Master’dan destek ve yardım alabilir.
+     */
+
+     // Sprint Backlog Nedir?
+    /*
+    * Bir sprintte yapılacak işlerin adımlandırılmış, detaylandırılmış, saatlendirilmiş içeriğin yer aldığı ve
+    Product Backlog’daki öncelik sırasına göre, tüm ekibin beraber  oluşturduğu bir listedir.
+    * Sprint Backlog, işlerin puanlarına ve takımın hızına (velocity) göre oluşturulur. Düşük öncelikli olanları
+    ilk sprint'lere koymazlar
+     */
+
+   // Scrum Ceremonies Nedir?
+     /*
+     4 tane Scrum meetings vardir.
+      */
+     // 1. Sprint Planning Meeting:
+    /*
+    * Bu meeting, her Sprint'i başlatan etkinliktir.
+    * PO ve Development ekibinin Sprint'e hangi Product Backlog lerı(PBI) dahil edileceğini tartıştığı toplantıdır.
+    **Sprint Planning toplantısının sonucu, herkesin kabul ettiği gerçekçi ve ulaşılabilir bir Sprint hedefi ve
+   Sprint Backlog elde etmektir.
+     */
+
+    // 2. Daily Scrum / Daily Standup Nedir?
+    /*
+    * Her gün yapilan ve team in birbirlerine bilgi verdiği toplantıdır. Bu toplantıya sadece Development team katılır.
+    * Genellikle kısa süren(yaklasik 15 dakika kadar) toplantıdır.
+    * Birçok takım bu toplantıyı kısa ve öz tutmak için yararlı bir teknik olarak bulmaktadır.
+    * Genelde dün neler yaptık? Bugün ne üzerinde çalışacağız? Her hangi bir sıkıntı veya engel ile karşılaştık mı?
+     */
+
+     // 3. Sprint Review – Sprint Demo Nedir?
+    /*
+    * Stakholders ve PO ın katıldığı bu toplantıdır.
+    *Bu toplantıda, Stakeholders calışan ürünü görerek bu ürüne eklenmesi  istedikleri yeni özellikler ya da
+    değiştirilmesini istedikleri yerlerin hangi niteliğe, görselliğe, yetkinliğe
+    sahip olması gerektiğini yüz yüze söyleyebilirler.
+     */
+
+    // 4. Sprint Retrospective - Sprint Retro Nedir?
+    /*
+    *Sprint'teki son toplantıdır ve “sprint review” toplantısının hemen ardından yapılır. (Stakeholders lar gidince)
+    * Scrum ekibi gelecekteki Sprint'ler için nelerin geliştirilebileceğini ve nasıl yapmaları gerektiğini gözden
+    geçirir.
+    * Önceki sprint in değerlendirilmesi yapılır.
+    * Bu toplantıya geliştirme ekibi, scrum master ve product owner katılır.
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
